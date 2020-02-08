2a8121e1c64275edec9d85aca5746ea3bd29246a
azambranog <dummymail@dummyprovide.com>
2020-02-08 17:20:40 +0100

    fix function

--- 2019/format_as_tables.R
+++ 2019/format_as_tables.R
@@ -10,8 +10,8 @@ format_as_table <- function(filename) {
   N <- slides[1]
   slides <- tail(slides, -1)
   
-  types <- sapply(as.list(slides), function(x) {gsub('^([^ ]*).*$', '\\1',slides[1])})
-  n_tags <- sapply(as.list(slides), function(x) {as.numeric(gsub('^([^ ]*) ([^ ]*).*$', '\\2',slides[1]))})
+  types <- sapply(as.list(slides), function(x) {gsub('^([^ ]*).*$', '\\1',x[1])})
+  n_tags <- sapply(as.list(slides), function(x) {as.numeric(gsub('^([^ ]*) ([^ ]*).*$', '\\2', x[1]))})
   tags <- sapply(as.list(slides), function(x) {tail(strsplit(x, ' ')[[1]], -2)})
   
   data <- data.table(orient = types, n_tags=n_tags, tags=tags)
--- 2019/vertical_counts.R
+++ 2019/vertical_counts.R
@@ -5,7 +5,9 @@ files <- list.files('data', pattern = 'txt$')
 for (f in files){
   message(f)
   data <- format_as_table(f)
+  
   write.table(data[, .N, by = orient], 
               file.path('processed', 'vertical_counts',f),
               row.names = F)
+  print(data[, .N, by = orient])
 }
