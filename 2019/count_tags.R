32bcd9a7139b5bf787c413e7deb516579e491749
azambranog <dummymail@dummyprovide.com>
2020-02-08 15:54:16 +0100

    fisrt submisison

---
+++ 2019/count_tags.R
@@ -0,0 +1,31 @@
+library(data.table)
+
+
+
+get_counts <- function(filename) {
+  
+  input_file <- file.path('data', filename)
+  
+  slides <- readLines(input_file)
+  
+  N <- slides[1]
+  slides <- tail(slides, -1)
+  
+  
+  tags <- sapply(as.list(slides), function(x) {tail(strsplit(x, ' ')[[1]], -2)})
+  
+  counts <- table(unlist(tags))
+  
+  counts <- as.data.table(counts)
+  setnames(counts, 'V1', 'tag')
+  
+  counts[order(N, decreasing = T)]
+  
+  write.table(counts, file.path('counts', filename), row.names = F)
+}
+
+files <- list.files('data', pattern = 'txt$')
+for (f in files){
+  message(f)
+  get_counts(f)
+}
